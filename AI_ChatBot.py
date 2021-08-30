b0908644644f160ea9aff7092eee29df59462693
Manal Mustafa <53543448+MenoMustafa@users.noreply.github.com>
2021-08-30 22:53:52 +0300

    Update AI_ChatBot.py

--- AI_ChatBot.py
+++ AI_ChatBot.py
@@ -1,80 +1,90 @@
 
+# Install important packages 
 pip install nltk
 pip install tflearn 
 
-
-import json
-import tensorflow as tf
+# Import libraries
+import json         # Upload and read json file
+import tensorflow as tf         #Build deep learning model
 import tflearn
-import numpy
-import random
-import nltk
-nltk.download('punkt')
+import numpy        # Store data
+import random       # Shuffle the training set
+import nltk         # For text analysis
+nltk.download('punkt')      # Download the pre-trained tokenized model for English language
 from nltk.stem.lancaster import LancasterStemmer
 stemm = LancasterStemmer()
 
+# Load training data
 from google.colab import files
-files.upload()
+files.upload()      # Upload intents file
 
-with open('intents.json') as json_data:
-    intents = json.load(json_data)
+with open('intents.json') as json_data:     # Open json file
+    intents = json.load(json_data)          # Save file in the intents object
 
-intents
-
+# Initialize files.
 words = []
 classes = []
 docs = []
-ignore = ['?']
+ignore = ['?']      # Ignore "?" symbol
+# Loop for each sentence
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        word = nltk.word_tokenize(pattern)
-        words.extend(word)
-        docs.append((word, intent['tag']))
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
+        word = nltk.word_tokenize(pattern)      # Tokenize each word
+        words.extend(word)      
+        docs.append((word, intent['tag']))      # Adding words to the documents
+        if intent['tag'] not in classes:        
+            classes.append(intent['tag'])       # Add classes to the class list
             
-words = [stemm.stem(word.lower()) for word in words if word not in ignore]
+words = [stemm.stem(word.lower()) for word in words if word not in ignore]      # Stem and lower each word, remove the duplicate words too
 words = sorted(list(set(words)))
-
+# Sort tags
 classes = sorted(list(set(classes)))
 
 print (len(docs), "Documents")
-print (len(classes), "Classes", classes)
-print (len(words), "Unique stemmed words", words)
+print (len(classes), "Classes", classes)        # Intent classes
+print (len(words), "Unique stemmed words", words)       # All words and vocabs
 
+# Initialize training data
 train = []
 output = []
-output_empty = [0] * len(classes)
+output_empty = [0] * len(classes)       # Initialize empty array for output
 
 for doc in docs:
-    bag = []
-    pattern_words = doc[0]
+    bag = []        # Initialize bag of words
+    pattern_words = doc[0]      # Tokenized words list for the pattern
     pattern_words = [stemm.stem(word.lower()) for word in pattern_words]
+    # Create array for bag of words with 1
     for word in words:
         bag.append(1) if word in pattern_words else bag.append(0)
-
+        
+    # For each pattern, the output is " 1 " for the current class, and " 0 " for each class 
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
+    
     train.append([bag, output_row])
 
-random.shuffle(train)
-train = numpy.array(train)
+random.shuffle(train)       # Shuffle the features
+train = numpy.array(train)      # Make numpy array
 
-train_x = list(train[:,0])
-train_y = list(train[:,1])
+# create training set and split it into features and labels
+train_x = list(train[:,0])      # Features- pattern
+train_y = list(train[:,1])      # Labels- tags
 
 
 from tensorflow.python.framework import ops
-ops.reset_default_graph()
+ops.reset_default_graph()       # Reset the global default graph
 
+# Build deep neural network model
 net = tflearn.input_data(shape=[None, len(train_x[0])])
 net = tflearn.fully_connected(net, 10)
 net = tflearn.fully_connected(net, 10)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
 
+# Define the model 
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 
+# Train and save the model
 model.fit(train_x, train_y, n_epoch=1000, batch_size=16, show_metric=True)
 model.save('model.tflearn')
 
@@ -82,17 +92,17 @@ import pickle
 pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
 
 def clean_sentence(sentence):
-    sentence_w = nltk.word_tokenize(sentence)
+    sentence_w = nltk.word_tokenize(sentence)       # Split words into arrays using tokenize algorithm
     sentence_w = [stemm.stem(word.lower()) for word in sentence_w]
     return sentence_w
   
 def bag_words(sentence, words, show_details=False):
-    sentence_w = clean_up_sentence(sentence)
+    sentence_w = clean_sentence(sentence)       # Tokenize pattern
     bag = [0]*len(words)  
-    for s in sentence_w:
+    for sentence in sentence_w:
         for i,w in enumerate(words):
-            if w == s: 
-                bag[i] = 1
+            if w == sentence: 
+                bag[i] = 1      # If the current word in the vocab position, assign 1
                 if show_details:
                     print ("found in bag: %s" % w)
 
@@ -101,7 +111,7 @@ def bag_words(sentence, words, show_details=False):
 
 ERROR_THRESHOLD = 0.30
 def classify(sentence):
-    results = model.predict([bow(sentence, words)])[0]
+    results = model.predict([bag_words(sentence, words)])[0]
     results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
